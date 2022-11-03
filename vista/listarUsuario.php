2350f95d306de6c06fe0c81671fda8d5ceaf4c43
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2022-11-03 16:12:33 -0300

    no se

--- vista/listarUsuario.php
+++ vista/listarUsuario.php
@@ -1,3 +1,33 @@
-<?php include_once ('common/header.php')?>
+<?php include_once('common/header.php') 
+    
+?>
 
-<?php include_once ('common/footer.php')?>
+<div class="container-md my-4 mt-5">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Usuario</th>
+                <th scope="col">Rol</th>
+            </tr>
+        </thead>
+        
+            <?php
+            $i = 0;
+            while ($i < $cantUsuarios) {
+            ?>
+                <tbody>
+                    <tr>
+                        <th>Usuario</th>
+                        <td>Rol</td>
+                    </tr>
+                </tbody>
+            <?php
+                $i++;
+            }
+            ?>
+        
+
+    </table>
+</div>
+
+<?php include_once('common/footer.php') ?>
