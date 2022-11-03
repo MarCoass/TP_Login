299acab3a780a0d38b22057e433b41d99308aea1
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2022-11-03 15:16:34 -0300

    Clase session

---
+++ control/Session.php
@@ -0,0 +1,17 @@
+<?php
+
+class Session{
+    //Constructor que inicia la sesion
+
+    //iniciar($nombreUsuario,$psw). Actualiza las variables de sesión con los valores ingresados.
+
+    //validar(). Valida si la sesión actual tiene usuario y psw  válidos. Devuelve true o false
+
+    //activa(). Devuelve true o false si la sesión está activa o no.
+
+    //getUsuario().Devuelve el usuario logeado.
+
+    //getRol(). Devuelve el rol del usuario  logeado.
+
+    //cerrar(). Cierra la sesión actual.
+}
