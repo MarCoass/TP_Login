6d3f007545cb142a9b0d5469d48b87e1cdd45f8f
MartinaRosales <martina.est.fi.uncoma.edu.ar>
2022-11-03 17:44:45 -0300

    modifique el footer

--- vista/common/footer.php
+++ vista/common/footer.php
@@ -1,5 +1,6 @@
-<footer class="page-footer font-small position-sticky bottom-0">
-  Equipo alfa buena onda 10.1
+<footer class="page-footer position-relative bottom-0">
+<div class="footer-copyright text-center py-3 text-light col mx-0 mt-2" id="copyright">Equipo Alfa Buena Onda 10.1 :)
+    </div>
 </footer>
 </body>
 </html>
--- vista/paginaSegura.php
+++ vista/paginaSegura.php
@@ -1,3 +1,6 @@
 <?php include_once ('common/header.php')?>
-
+<div class="container mx-auto" style="margin:30px;height:0vh;"  id="pagSegura"></div>
+    <h1 id=tituloSeguro class="text-light mt-3 mb-3">Bienvenidx a la p&aacute;gina segura :)</h1>
+    <img class="position-relative start-50 translate-middle-x mt-3" src="Assets/Img/memegatito.png" alt="meme de gatito">
+</div>
 <?php include_once ('common/footer.php')?>
